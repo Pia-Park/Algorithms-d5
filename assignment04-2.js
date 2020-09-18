86579fbddbc3198c6a020ea27f40fb095f2a2b4b
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-09-18 12:20:54 -0700

    changed third answer

--- assignment04-2.js
+++ assignment04-2.js
@@ -1,7 +1,34 @@
 // Write me a function that converts any integer(up to 255) into binary. 
 
+
 function convertTobinary(num){
-    return (num >>> 0).toString(2);
+    let binary = "";
+    if (num < 0) {
+      num = num >>> 0;
+    }
+    while(Math.ceil(num / 2) > 0){
+        binary = num % 2 + binary;
+        num = Math.floor(num / 2);
+    }
+    return binary;
 }
 
+ 
+
 console.log(convertTobinary(255));
+
+
+// function convertTobinary(num){
+//     return (num >>> 0).toString(2);
+// }
+
+// const convertTobinary = function(num){
+//     let result = [];
+//     let number = num;
+
+//     while(number >= 1){
+//         result.unshift(Math.floor(num % 2));
+//         number = number / 2;
+//     }
+//     return result;
+// }
